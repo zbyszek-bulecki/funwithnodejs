c75423eff031060007b174264c200690c5185884
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-07-29 15:42:07 +0200

    initial server code created

---
+++ server.js
@@ -0,0 +1,11 @@
+const WebSocketServer = require('ws');
+const server = new WebSocket.Server({port: '8080'})
+
+server.on('connection', socket => {
+
+    socket.on('message', message => {
+
+        socket.send('Roger that! $(message)');
+    });
+
+});
